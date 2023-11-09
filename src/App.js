030c6388356e5ee7c1931dc2c2ddc3df73b3870d
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-09 16:36:38 +0430

    a travel pack list app with react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "charger", quantity: 12, packed: true }
+];
+
+
+
 export default function App() {
   return <div className="app">
     <Logo />
@@ -12,20 +20,48 @@ function Logo() {
 }
 
 function Form() {
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-    </div>
+
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form >
   );
 }
 
 function PackingList() {
-  return <div className="list">List</div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item(item) {
+
+  return <li>{item.description}</li>
+
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have x items on your list, and you alreardy packed x (X%)</em>
     </footer>
   )
